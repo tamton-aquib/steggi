7e840bf3a26bb02122768f79c5ac62c710eabc69
Aquib <aquibjavedt007@gmail.com>
2023-01-10 19:57:00 +0530

    version bump + imports fixed

--- steggi/steggi.py
+++ steggi/steggi.py
@@ -1,6 +1,6 @@
 import cv2
 import tkinter as tk
-from steggi.models import converters, extractors, combiners
+from steggi import converters, extractors, combiners
 from tkinter import filedialog
 from PIL import ImageTk, Image
 
